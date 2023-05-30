b1c632ab8230c238160c7f6b2d44266bb13783f9
Prathamesh Sirdesai <sirdesai.work@gmail.com>
2023-05-30 17:19:45 +0530

    first commit

---
+++ src/ChatMessage.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let message;
+  export let sender;
+
+  const messageClass = message.who === sender ? 'sent' : 'received';
+
+  const avatar = `https://source.unsplash.com/400x400/?${message.who},avatar`;
+
+  const ts = new Date(message.when);
+
+  //Time set too dissapear message
+  setTimeout(() => {
+    const messageElements = document.querySelectorAll('.message');
+    messageElements.forEach((element) => {
+      element.remove();
+    });
+  }, 60000);
+
+</script>
+
+<div class={`message ${messageClass}`}>
+  <img src={avatar} alt="avatar" />
+  <div class="message-text">
+    <p>{message.what}</p>
+
+    <time>{ts.toLocaleTimeString()}</time>
+  </div>
+</div>
---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>#Hash</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Signed in as <strong>{$username}</strong></span>
+      <!-- <img src={`https://source.unsplash.com/400x400/?blackavatar`} alt="avatar" />  -->
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3>D-Chat App</h3>
+  {/if}
+</header>
